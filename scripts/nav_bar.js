a698a242509d4b9bca32d670ba67d617a800ecb7
Szymon Motała <szymon.motala95@gmail.com>
2022-01-02 02:43:56 +0100

    test

--- scripts/nav_bar.js
+++ scripts/nav_bar.js
@@ -2,6 +2,7 @@ const items = document.querySelectorAll(".nav_item a");
 
 items.forEach((item) => {
   const href = item.getAttribute("href");
+  console.log(window.location);
   if (window.location.href.includes(href)) {
     item.classList.add("active");
   } else if (window.location.pathname == "/" && href == "index.html") {
