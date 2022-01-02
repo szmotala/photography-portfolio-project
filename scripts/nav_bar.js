9ee89ecd7c31b374c54b032eb816feb232225de5
Szymon Motała <szymon.motala95@gmail.com>
2022-01-02 16:29:14 +0100

    convert all styles to scss

--- scripts/nav_bar.js
+++
@@ -1,8 +0,0 @@
-const items = document.querySelectorAll(".nav_item a");
-
-items.forEach((item) => {
-  const href = item.getAttribute("href");
-  if (window.location.href.includes(href)) {
-    item.classList.add("active");
-  }
-});
